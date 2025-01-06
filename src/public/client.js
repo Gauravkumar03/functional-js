c17af4d9aa26ad16d916c714892e4fa78bcbfa1b
Gaurav Kumar <gauravkmadhubani@gmail.com>
2025-01-06 17:31:55 +0530

    completed

--- src/public/client.js
+++ src/public/client.js
@@ -1,18 +1,18 @@
-let store = {
-    // user: { name: "Student" },
-    // apod: '',
-    // rovers: ['Curiosity', 'Opportunity', 'Spirit'],
+let store = Immutable.Map({
     curiosityData: {},
     opportunityData: {},
     spiritData: {}
-}
+})
 
-// add our markup to the page
+//creating a variable finalStore which becomes immutable after data is fetched from the api
+let finalStore
+
+// // add our markup to the page
 const root = document.getElementById('root')
 
-const updateStore = (store, newState) => {
-    store = Object.assign(store, newState)
-    render(root, store)
+const updateStore = (newState) => {
+    finalStore = store.merge(newState)
+    render(root, finalStore)
 }
 
 const render = async (root, state, rover) => {
@@ -20,18 +20,18 @@ const render = async (root, state, rover) => {
 }
 
 
-// create content
+// // create content
 const App = (state, rover) => {
-    //let { Curiosity, Opportunity, Spirit } = state
-
     return `
         <header></header>
         <main>
             <section>
-                <h3>Put things on the page!</h3>
-                <button onclick='curiosityFun(event)'>Curiosity</button>
-                <button onclick='opportunityFun()'>Opportunity</button>
-                <button onclick='spiritFun()'>Spirit</button>
+                <h1>Welcome To Nasa</h1>
+                <ul>
+                <li onclick='curiosityFun(event)'>Curiosity</li>
+                <li onclick='opportunityFun(event)'>Opportunity</li>
+                <li onclick='spiritFun(event)'>Spirit</li>
+            </ul> 
                 ${RoverImage(state, rover)}
             </section>
         </main>
@@ -45,84 +45,74 @@ window.addEventListener('load', () => {
 })
 
 function curiosityFun(event) {
-    render(root, store, event.target.textContent)
-    console.log(store.curiosityData.latest_photos[0])
-    console.log(store)
+    render(root, finalStore, event.target.textContent)
 }
 
-function opportunityFun() {
-    render(root, store, event.target.textContent)
-    console.log(store.opportunityData.latest_photos[0])
+function opportunityFun(event) {
+    render(root, finalStore, event.target.textContent)
 }
 
-function spiritFun() {
-    render(root, store, event.target.textContent)
-    console.log(store.spiritData.latest_photos[0])
+function spiritFun(event) {
+    render(root, finalStore, event.target.textContent)
 }
 
-// ------------------------------------------------------  COMPONENTS
+// // ------------------------------------------------------  COMPONENTS
 
-// Example of a pure function that renders infomation requested from the backend
+//Another HOF that renders UI elements.
+function renderUI(callback, launch, landing, status, recent, imgUrl) {
+    return callback(launch, landing, status, recent, imgUrl)
+}
+
+function renderLayout(launch, landing, status, recent, imgUrl) {
+    return `
+        <p>Launch Date: ${launch}</p>
+        <p>Landing Date: ${landing}</p>
+        <p>Status: ${status}</p>
+        <p>Recent Available Photo's Date: ${recent}</p>
+        <br>
+        <img src=${imgUrl} style='width: 200px; height: 200px'>
+        `
+}
+
+// // Example of a pure function that renders infomation requested from the backend
 const RoverImage = (state, rover) => {
-
-    if (Object.keys(state.curiosityData).length === 0) {
-        fetchRover(store)
+    if (finalStore === undefined) {
+        fetchRover()
         return `<p>...Loading Rover's Data</p>`
     } else if (rover === 'Curiosity') {
-        return `
-        <p>Launch Date: ${state.curiosityData.latest_photos[0].rover.launch_date}</p>
-        <p>Landing Date: ${state.curiosityData.latest_photos[0].rover.landing_date}</p>
-        <p>Status: ${state.curiosityData.latest_photos[0].rover.status}</p>
-        <p>Recent Available Photo's Date: ${state.curiosityData.latest_photos[0].earth_date}</p>
-        <br>
-        <img src=${state.curiosityData.latest_photos[0].img_src} alt='Curiosity Image' style='width: 200px; height: 200px'>
-        `
+        const launch = state.get('curiosityData').latest_photos[0].rover.launch_date
+        const landing = state.get('curiosityData').latest_photos[0].rover.landing_date
+        const status = state.get('curiosityData').latest_photos[0].rover.status
+        const recent = state.get('curiosityData').latest_photos[0].earth_date
+        const imgUrl = state.get('curiosityData').latest_photos[0].img_src 
+        return renderUI(renderLayout,launch, landing, status, recent, imgUrl)
 
     } else if (rover === 'Opportunity') {
-        return `
-        <p>Launch Date: ${state.opportunityData.latest_photos[0].rover.launch_date}</p>
-        <p>Landing Date: ${state.opportunityData.latest_photos[0].rover.landing_date}</p>
-        <p>Status: ${state.opportunityData.latest_photos[0].rover.status}</p>
-        <p>Recent Available Photo's Date: ${state.opportunityData.latest_photos[0].earth_date}</p>
-        <br>
-        <img src=${state.opportunityData.latest_photos[0].img_src} alt='Curiosity Image' style='width: 200px; height: 200px'>
-        `
+        const launch = state.get('opportunityData').latest_photos[0].rover.launch_date
+        const landing = state.get('opportunityData').latest_photos[0].rover.landing_date
+        const status = state.get('opportunityData').latest_photos[0].rover.status
+        const recent = state.get('opportunityData').latest_photos[0].earth_date
+        const imgUrl = state.get('opportunityData').latest_photos[0].img_src 
+        return renderUI(renderLayout,launch, landing, status, recent, imgUrl)
     } else if (rover === 'Spirit') {
-        return `
-        <p>Launch Date: ${state.spiritData.latest_photos[0].rover.launch_date}</p>
-        <p>Landing Date: ${state.spiritData.latest_photos[0].rover.landing_date}</p>
-        <p>Status: ${state.spiritData.latest_photos[0].rover.status}</p>
-        <p>Recent Available Photo's Date: ${state.spiritData.latest_photos[0].earth_date}</p>
-        <br>
-        <img src=${state.spiritData.latest_photos[0].img_src} alt='Curiosity Image' style='width: 200px; height: 200px'>
-        `
+        const launch = state.get('spiritData').latest_photos[0].rover.launch_date
+        const landing = state.get('spiritData').latest_photos[0].rover.landing_date
+        const status = state.get('spiritData').latest_photos[0].rover.status
+        const recent = state.get('spiritData').latest_photos[0].earth_date
+        const imgUrl = state.get('spiritData').latest_photos[0].img_src 
+        return renderUI(renderLayout,launch, landing, status, recent, imgUrl)
     } else {
         return `<p>Loading Complete. Click the button to see the images of rovers you want.</p>`
     }
-
-    // check if the photo of the day is actually type video!
-    // if (apod.media_type === "video") {
-    //     return (`
-    //         <p>See today's featured video <a href="${apod.url}">here</a></p>
-    //         <p>${apod.title}</p>
-    //         <p>${apod.explanation}</p>
-    //     `)
-    // } else {
-    //     return (`
-    //         <img src="${apod.image.url}" height="350px" width="100%" />
-    //         <p>${apod.image.explanation}</p>
-    //     `)
-    // }
     
 }
 
-// ------------------------------------------------------  API CALLS
+// // ------------------------------------------------------  API CALLS
 
-// Example API call
-const fetchRover = (state) => {
+// // Example API call
+const fetchRover = () => {
     console.log('entering this')
     fetch(`http://localhost:3000/roversData`)
         .then(res => res.json())
-        .then(data => updateStore(store, data))
-        //.then(apod => updateStore(store, { apod }))
+        .then(data => updateStore(data))
 }
